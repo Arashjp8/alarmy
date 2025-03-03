0c5c592b10afb20722bbfdf21d05e0b7023644cb
arashjp8 <arashjafarpour723@gmail.com>
2025-03-03 17:31:27 +0330

    [initial] - first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import tw from "twrnc";
+
+export default function Index() {
+    return (
+        <View style={tw`bg-slate-900 flex-1 justify-center items-center gap-2`}>
+            <Text style={tw`text-white text-lg`}>Moo 🐮</Text>
+            <Text style={tw`text-white text-lg`}>Moo 🐮</Text>
+        </View>
+    );
+}
